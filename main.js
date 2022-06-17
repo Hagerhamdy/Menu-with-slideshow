b90306d4c457b4c38c8343f79a938a904ec82292
Hagerhamdy <hagerhamdee224@gmail.com>
2022-06-17 20:04:24 +0200

    slideshow

---
+++ main.js
@@ -0,0 +1,18 @@
+// let slideImage = ["images/1.jpg", "images/2.webp", "images/3.avif"];
+// let i =0;
+
+// let slideshow = function(){
+//     document.slideImg.src = slideImage[i];
+//     if(i < slideImage.length -1){
+//         i++;
+//     }
+//     else{
+//         i = 0;
+//     }
+//     setTimeout("slideshow()", 2000);
+// }
+
+// slideshow();
+
+
+
